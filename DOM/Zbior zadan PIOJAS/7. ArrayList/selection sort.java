5666dc722fdc5c8dd2c30c7deb79bde3ce1d68ed
Igor Barcikowski <150329028+Barcik04@users.noreply.github.com>
2024-10-10 18:21:42 +0200

    Update selection sort.java

--- DOM/Zbior zadan PIOJAS/7. ArrayList/selection sort.java
+++ DOM/Zbior zadan PIOJAS/7. ArrayList/selection sort.java
@@ -1,1 +1,29 @@
+import java.util.ArrayList;
+import java.util.Random;
 
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        Random rand = new Random();
+
+        for (int i = 0; i < 10; i++) {
+            list.add(rand.nextInt(50, 100));
+        }
+
+        System.out.println(sortArray(list));
+    }
+
+    public static ArrayList<Integer> sortArray(ArrayList<Integer> list) {
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                if (list.get(i) > list.get(j)) {
+                    int temp = list.get(i);
+                    list.set(i, list.get(j));
+                    list.set(j, temp);
+                }
+            }
+        }
+
+        return list;
+    }
+}
