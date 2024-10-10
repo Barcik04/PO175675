21428bfba29178106c65ca170153a0fe16850f4e
Igor Barcikowski <150329028+Barcik04@users.noreply.github.com>
2024-10-10 19:09:46 +0200

    Create 1.java

---
+++ DOM/Zbior zadan PIOJAS/8. Napisy/1.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        String word = "roleX";
+        char[] chars = word.toCharArray();
+
+        for (int i = 0; i < chars.length / 2; i++) {
+            char temp = chars[i];
+            chars[i] = chars[chars.length - 1 - i];
+            chars[chars.length - 1 - i] = temp;
+        }
+
+        System.out.println(chars);
+    }
+}
