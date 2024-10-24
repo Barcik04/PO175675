6255fab40d4b940b759320b09531cd78219e9069
Igor Barcikowski <150329028+Barcik04@users.noreply.github.com>
2024-10-24 19:04:13 +0200

    Create 3.java

---
+++ DOM/Zbior zadan PIOJAS/12.Pola i metody statyczne/3.java
@@ -0,0 +1,21 @@
+public class Singleton {
+    private static Singleton instance = null;
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+
+    public static void main(String[] args) {
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+        System.out.println(s1 == s2);
+        System.out.println(s1);
+        System.out.println(s2);
+    }
+}
